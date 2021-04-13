185c6cbaea2d9977435443daf6ddd6423ce2130d
ZHT <a2862058843@163.com>
2021-04-13 09:10:55 +0800

    debug

---
+++ lib/laudio/demo04.dart
@@ -0,0 +1,9 @@
+// import 'dart:async';
+// import 'package:flutter_audio_recorder/flutter_audio_recorder.dart';
+// import 'package:audioplayers/audioplayers.dart';
+// import 'package:path_provider/path_provider.dart';
+//
+// void hasPermission() async {
+//   bool hasPermission = await FlutterAudioRecorder.hasPermissions;
+// }
+//
--- lib/laudio/demo05.dart
+++ lib/laudio/demo05.dart
@@ -1,79 +1,95 @@
+/*
+ * Copyright 2018, 2019, 2020 Dooboolab.
+ *
+ * This file is part of Flutter-Sound.
+ *
+ * Flutter-Sound is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 (LGPL-V3), as published by
+ * the Free Software Foundation.
+ *
+ * Flutter-Sound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Flutter-Sound.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SimplePlayback(),
+  ));
+}
+
+/*
+ *
+ * This is a very simple example for Flutter Sound beginners,
+ * that show how to record, and then playback a file.
+ *
+ * This example is really basic.
+ *
+ */
+
+final _exampleAudioFilePathMP3 =
+    'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3';
 
 ///
 typedef Fn = void Function();
 
-///
-/// 播放、录音
-///
-void main() {
-  runApp(MaterialApp(
-    home: MyRecorderApp(),
-  ));
-}
-
-class MyRecorderApp extends StatefulWidget {
+/// Example app.
+class SimplePlayback extends StatefulWidget {
   @override
-  _MyRecorderAppState createState() => _MyRecorderAppState();
+  _SimplePlaybackState createState() => _SimplePlaybackState();
 }
 
-class _MyRecorderAppState extends State<MyRecorderApp> {
+class _SimplePlaybackState extends State<SimplePlayback> {
   FlutterSoundPlayer _mPlayer = FlutterSoundPlayer();
-
   bool _mPlayerIsInited = false;
 
   @override
   void initState() {
     super.initState();
-    //播放器
-    _mPlayer?.openAudioSession().then((value){
+    _mPlayer?.openAudioSession().then((value) {
       setState(() {
-        _mPlayerIsInited=true;
+        _mPlayerIsInited = true;
       });
     });
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: new AppBar(title: new Text("播放音频"),),
-  //     body: new Container(
-  //       child: new ElevatedButton(onPressed: (){
-  //         if(_mPlayerIsInited) play();
-  //       }, child: Icon(Icons.play_arrow)),
-  //     ),
-  //   );
-  // }
-  void play() async {
-    await _mPlayer.startPlayer(
-        fromURI: "'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3",
-        codec: Codec.mp3,
-        whenFinished: (){setState((){
+  @override
+  void dispose() {
+    stopPlayer();
+    // Be careful : you must `close` the audio session when you have finished with it.
+    _mPlayer?.closeAudioSession();
+    _mPlayer = null;
 
-        });}
-    );
+    super.dispose();
+  }
+
+  // -------  Here is the code to playback a remote file -----------------------
+
+  void play() async {
+    await _mPlayer?.startPlayer(
+        fromURI: _exampleAudioFilePathMP3,
+        codec: Codec.mp3,
+        whenFinished: () {
+          setState(() {});
+        });
     setState(() {});
   }
 
   Future<void> stopPlayer() async {
     if (_mPlayer != null) {
-      await _mPlayer.stopPlayer();
+      await _mPlayer?.stopPlayer();
     }
   }
 
-  @override
-  void dispose(){
-    /**
-     * 关闭会话
-     */
-    stopPlayer();
-    if(_mPlayer!=null){
-      _mPlayer.closeAudioSession();
-      _mPlayer=null;
-    }
-    super.dispose();
-  }
+  // --------------------- UI -------------------
 
   Fn getPlaybackFn() {
     if (!_mPlayerIsInited) {
